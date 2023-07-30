77afcd456c5ae74cadc3ae5fe6d9829a0a573dbb
niukaifang <kaifang.niu@dpvr.com>
2023-07-30 17:46:00 +0800

    2.1.2
    create single point show

--- computer_graphics_programming_in_opengl/chapter_2/chapter_2.cpp
+++ computer_graphics_programming_in_opengl/chapter_2/chapter_2.cpp
@@ -2,13 +2,54 @@
 #include "GLFW/glfw3.h"
 #include <iostream>
 
+#define numVAOs 1
+
+GLuint  renderingProgram;
+GLuint  vao[numVAOs];
+
+GLuint createShaderProgram()
+{
+    const char* vshaderSource =
+        "#version 330 core\n"
+        "void main(void) \n"
+        "{gl_Position = vec4(0.0, 0.0, 0.0, 1.0);}";
+
+    const char* fshaderSource =
+        "#version 330 core\n"
+        "out vec4 color; \n"
+        "void main(void) \n"
+        "{color = vec4(0.0, 0.0, 1.0, 1.0);}";
+
+    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(vShader, 1, &vshaderSource, NULL);
+    glShaderSource(fShader, 1, &fshaderSource, NULL);
+    glCompileShader(vShader);
+    glCompileShader(fShader);
+
+    GLuint vfProgram = glCreateProgram();
+    glAttachShader(vfProgram, vShader);
+    glAttachShader(vfProgram, fShader);
+    glLinkProgram(vfProgram);
+
+    return vfProgram;
+}
+
 using namespace std;
 
-void init(GLFWwindow* window) {};
+void init(GLFWwindow* window)
+{
+    renderingProgram = createShaderProgram();
+    glGenVertexArrays(numVAOs, vao);
+    glBindVertexArray(vao[0]);
+}
+
 void display(GLFWwindow* window, double currTime)
 {
-    glClearColor(1.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(renderingProgram);
+    glPointSize(30.f);
+    glDrawArrays(GL_POINTS, 0, 1);
 }
 
 int main()
